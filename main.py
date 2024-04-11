4c7318d06ef32d5f2fd5ff07e30e18a5d7cf37a5
MeltedButter <meltedbuttergaming@gmail.com>
2024-04-11 17:26:39 +0930

    Adjust boids' distance to neighbors and walls.

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ class Boid(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(center=self.pos)
 
     def update(self):
-        self.pos += pygame.Vector2(math.cos(self.angle), math.sin(self.angle)) * speed
+        self.pos += pygame.Vector2(math.cos(self.angle), -math.sin(self.angle)) * speed
         self.rect.center = self.pos
 
         for boid in self.game.boids:
             if boid == self:
                 continue
-            if self.pos.distance_to(boid.pos) < 60:
+            if self.pos.distance_to(boid.pos) < 30:
                 # average their angles
                 average = (self.angle + boid.angle) / 2
                 if self.angle < average - turn_speed:
@@ -35,10 +35,38 @@ class Boid(pygame.sprite.Sprite):
                 elif self.angle > average + turn_speed:
                     self.angle -= 0.01
 
-            # turn away from walls
-            if self.pos.x < 0 or self.pos.x > 800 or self.pos.y < 0 or self.pos.y > 600:
-                self.angle += 0.01
+            if self.pos.distance_to(boid.pos) <= self.rect.x:
+                # point angles away from each-other
+                pass
 
+
+        # turn away from walls
+        offset = 60
+        if self.pos.x < 0 + offset:
+            if self.angle >= math.pi:
+                self.angle += turn_speed
+            elif self.angle < math.pi:
+                self.angle -= turn_speed
+        elif self.pos.x > self.game.screen.get_width() - offset:
+            if self.angle <= math.pi:
+                self.angle += turn_speed
+            elif self.angle < math.pi * 2:
+                self.angle -= turn_speed
+        elif self.pos.y < 0 + offset:
+            if self.angle >= math.pi / 2:
+                self.angle += turn_speed
+            elif self.angle < math.pi / 2 or self.angle > math.pi + (math.pi / 2): # less than up, or more than right (zero is to the right)
+                self.angle -= turn_speed
+        elif self.pos.y > self.game.screen.get_height() - offset:
+            if self.angle >= math.pi + (math.pi / 2) or self.angle < (math.pi / 2): # less than down, or less than up (zero is to the right)
+                self.angle += turn_speed
+            elif self.angle < math.pi + (math.pi / 2):
+                self.angle -= turn_speed
+
+        if self.angle > math.pi * 2:
+            self.angle -= math.pi * 2
+        if self.angle < 0:
+            self.angle += math.pi * 2
 
 class Game:
     def __init__(self):
@@ -53,7 +81,7 @@ class Game:
         self.boids.draw(self.screen)
         if debug:
             for boid in self.boids:
-                pygame.draw.line(self.screen, "black", boid.pos, boid.pos + pygame.Vector2(math.cos(boid.angle), math.sin(boid.angle)) * 50)
+                pygame.draw.line(self.screen, "black", boid.pos, boid.pos + pygame.Vector2(math.cos(boid.angle), -math.sin(boid.angle)) * 50)
 
         pygame.display.update()
 
