e24cce9a7c66d97cd4927269c2c0ccf3b4e287f3
MeltedButter77 <meltedbuttergaming@gmail.com>
2024-09-04 19:18:42 +0930

    Rewrite

--- main.py
+++ main.py
@@ -1,127 +1,78 @@
+import pygame
 import math
 import random
-import pygame
 
-debug = False
-speed = 1
-turn_speed = math.pi / 180 * 1
-vision_distance = 100
-wall_offset = 100
-simulation_speed = 200  # updates per second
-screen_size = (1000, 1000)
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock()
 
+boid_list = []
 
-class Boid(pygame.sprite.Sprite):
-    def __init__(self, game, pos, *groups: pygame.sprite.Group):
-        super().__init__(*groups)
+class Boid:
+    def __init__(self, x, y):
+        self.pos = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(random.randint(0, 100)/100, random.randint(0, 100)/100)
+        self.total_velocity = pygame.Vector2(0, 0)
+        self.rect = pygame.Rect(x, y, 10, 10)
 
-        self.evadingWall = False
-        self.game = game
-        self.pos = pygame.Vector2(pos)
-        self.angle = math.radians(random.randint(0, 360))
-        self.image = pygame.Surface((10, 10))
-        self.image.fill("blue")
-        self.rect = self.image.get_rect(center=self.pos)
+        boid_list.append(self)
 
     def update(self):
-        self.pos += pygame.Vector2(math.cos(self.angle), -math.sin(self.angle)) * speed
+        other_boids = boid_list.copy()
+        other_boids.remove(self)
+        if len(other_boids) > 0:
+            # Calculate the cohesion vector
+            total_locations = pygame.Vector2(0, 0)
+            for boid in other_boids:
+                total_locations += boid.pos
+            average_position = total_locations // len(other_boids)
+
+            cohesion_vector = pygame.Vector2(average_position - self.pos)
+
+
+            # Calculate the alignment vector
+            total_velocity = pygame.Vector2(0, 0)
+            for boid in other_boids:
+                total_velocity += boid.velocity
+            average_velocity = total_velocity // len(other_boids)
+
+            alignment_vector = pygame.Vector2(average_velocity - self.pos)
+
+            cohesion_vector.scale_to_length(1)
+            alignment_vector.scale_to_length(1)
+
+            self.total_velocity += cohesion_vector
+            self.total_velocity += alignment_vector
+
+            if cohesion_vector.length() >= 1:
+                print("yes")
+
+        self.total_velocity += self.velocity
+
+        self.velocity = self.total_velocity // 3
+
+        self.total_velocity = pygame.Vector2(0, 0)
+
+        if self.velocity.length() > 1:
+            self.velocity.scale_to_length(1)
+
+        self.pos += self.velocity
         self.rect.center = self.pos
 
-        for boid in self.game.boids:
-            if boid == self:
-                continue
+    def draw(self, screen):
+        pygame.draw.rect(screen, (255, 255, 255), self.rect)
 
-            if self.pos.distance_to(boid.pos) < vision_distance and not self.evadingWall:
-                # average their angles
-                average = (self.angle + boid.angle) / 2
-                if self.angle < average:
-                    self.angle += turn_speed
-                elif self.angle > average:
-                    self.angle -= turn_speed
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            Boid(event.pos[0], event.pos[1])
 
-            if self.pos.distance_to(boid.pos) <= self.rect.x:
-                # point angles away from each-other as to not collide
-                pass
+    for boid in boid_list:
+        boid.update()
+        boid.draw(screen)
 
-        # turn away from walls
-        if self.pos.x < 0 + wall_offset:
-            self.evadingWall = True
-            if self.angle >= math.pi:
-                self.angle += turn_speed
-            elif self.angle < math.pi:
-                self.angle -= turn_speed
-        elif self.pos.x > self.game.screen.get_width() - wall_offset:
-            self.evadingWall = True
-            if self.angle <= math.pi:
-                self.angle += turn_speed
-            elif self.angle < math.pi * 2:
-                self.angle -= turn_speed
-        elif self.pos.y < 0 + wall_offset:
-            self.evadingWall = True
-            if self.angle >= math.pi / 2:
-                self.angle += turn_speed
-            elif self.angle < math.pi / 2 or self.angle >= math.pi + (
-                    math.pi / 2):  # less than up, or more than right (zero is to the right)
-                self.angle -= turn_speed
-        elif self.pos.y > self.game.screen.get_height() - wall_offset:
-            self.evadingWall = True
-            if self.angle >= math.pi + (math.pi / 2) or self.angle < (
-                    math.pi / 2):  # less than down, or less than up (zero is to the right)
-                self.angle += turn_speed
-            elif self.angle < math.pi + (math.pi / 2):
-                self.angle -= turn_speed
-        else:
-            self.evadingWall = False
-
-        # collide with walls
-        if self.pos.x < 0:
-            self.pos.x = 0
-        if self.pos.x > self.game.screen.get_width():
-            self.pos.x = self.game.screen.get_width()
-        if self.pos.y < 0:
-            self.pos.y = 0
-        if self.pos.y > self.game.screen.get_height():
-            self.pos.y = self.game.screen.get_height()
-
-        if self.angle > math.pi * 2:
-            self.angle -= math.pi * 2
-        if self.angle < 0:
-            self.angle += math.pi * 2
-
-
-class Game:
-    def __init__(self):
-        self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode(screen_size)
-
-        self.boids = pygame.sprite.Group()
-
-    def render(self):
-        self.screen.fill((255, 255, 255))
-
-        if debug:
-            for boid in self.boids:
-                pygame.draw.circle(self.screen, "black", boid.pos, vision_distance)
-                pygame.draw.line(self.screen, "red", boid.pos, boid.pos + pygame.Vector2(math.cos(boid.angle), -math.sin(boid.angle)) * 50)
-        self.boids.draw(self.screen)
-
-        pygame.display.update()
-
-    def update(self):
-        self.boids.update()
-
-    def start(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    Boid(self, pygame.mouse.get_pos(), self.boids)
-
-            self.update()
-            self.render()
-            self.clock.tick(simulation_speed)
-
-
-Game().start()
+    pygame.display.update()
+    screen.fill((60, 60, 60))
+    clock.tick(60)
